d3617d7a85ab6a4ff4151cb8fa90f365b5cfa184
Khalid Saifullah <khalideofficial@gmail.com>
2021-05-24 12:43:43 +0600

    Little fixes

--- firebase.js
+++ firebase.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAbhucudE4NX2f0DOFdl3FABGKBa2urhjU",
-    authDomain: "amaz-clone-ks.firebaseapp.com",
-    projectId: "amaz-clone-ks",
-    storageBucket: "amaz-clone-ks.appspot.com",
-    messagingSenderId: "379541826028",
-    appId: "1:379541826028:web:4848bdf4e86000949a38c9"
+    // you firebase configs
 }
 
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
